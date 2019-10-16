744dbd382610c5b6fff21fb0398d47124a536c30
Tyler Fasulo <tfasulo10@gmail.com>
2019-10-15 22:25:11 -0400

    Web page

---
+++ Homework/web-design.py
@@ -0,0 +1,5 @@
+import csv
+
+with open('') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=','
+
---
+++ web-design.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Web Design</title>
+</head>
+
+<body>
+  <section>
+    <h1>Web Design</h1>
+    <article>
+    <a target=_blank href=C:\Users\Owner\Desktop\Homework-\11-Web\Homework\Instructions\Images\comparison-lg.png>
+      <img src=C:\Users\Owner\Desktop\Homework-\11-Web\Homework\Instructions\Images\comparison-lg.png alt="web design">
+  </section>
+</body>
+
+</html>
